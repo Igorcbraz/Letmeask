c2dc6b759e82cd2800e0d206b53b22d6e94e75d0
Igor Costa Braz <Igorcbraz1@gmail.com>
2021-09-14 15:26:49 -0300

    Inicio do projeto

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from './components/Button'
+
+function App() {
+  return (
+    <div>
+      <h1>oipoo</h1>
+      <Button />
+      <Button /> 
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
